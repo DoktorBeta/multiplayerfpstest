7658f00928735d418028599a160d07dcca1648b4
DoktorBeta <betababy@igame3d.com>
2017-05-19 20:49:54 -0600

    First commit
    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour {
+
+	public float groundCheckLength = 1.2f;
+
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3f;
+
+	private PlayerMotor motor;
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update()
+	{
+		float _xMov = Input.GetAxisRaw ("Horizontal");
+		float _zMov = Input.GetAxisRaw ("Vertical");
+
+		//get individual axis velocities from input
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		//define velocity
+		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+		//send velocity to motor
+		motor.Move (_velocity);
+
+		//Calculate rotation as a 3d vector. turning left and right
+		float _yRot = Input.GetAxisRaw("Mouse X");
+
+		Vector3 _rotation = new Vector3 (0f, _yRot, 0f) * lookSensitivity;
+
+		//Apply rotation
+		motor.Rotate(_rotation);
+
+		//Calculate camera rotation as a 3d vector
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		Vector3 _cameraRotation = new Vector3 (_xRot, 0f, 0f) * lookSensitivity;
+
+		//Apply rotation
+		motor.RotateCamera(_cameraRotation);
+
+		Vector3 forward = transform.TransformDirection (-Vector3.up);
+
+		if (Input.GetButton ("Jump") == true && Physics.Raycast (transform.position, -Vector3.up, groundCheckLength)) {
+			Debug.DrawRay (transform.position, forward * groundCheckLength, Color.green );
+			motor.jump = true;
+		} else {
+			Debug.DrawRay (transform.position, forward * groundCheckLength, Color.green );
+			motor.jump = false;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerHandController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandController : MonoBehaviour {
+
+	public float dragDist = 0.2f;
+	public float dragSpeed = 0.2f;
+
+	private bool isLooking;
+
+	public Vector3 targetPos;
+	public Vector3 pastPos;
+
+	public float xMouse;
+	public float yMouse;
+
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void FixedUpdate()
+	{
+		
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float xMouse = Input.GetAxisRaw ("Mouse X");
+		float yMouse = Input.GetAxisRaw ("Mouse Y");
+
+		if (xMouse != 0 || yMouse != 0) {
+			targetPos = new Vector3 (-xMouse * dragDist, -yMouse * dragDist, 0);
+		} else {
+			targetPos = new Vector3 (0, 0, 0);
+		}
+		// SMOOTHDAMP. IT WORKS. DON'T LERP, IT DOESN'T WORK.
+		transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPos, ref velocity, dragSpeed);
+	}
+		
+}
